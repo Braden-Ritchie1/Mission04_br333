a0e0e631a57ea774f6cbf37f08cbb6381f2bdc6e
Braden Ritchie <br333@byu.edu>
2023-02-06 13:15:01 -0700

    last push for mission 5

--- Mission04_br333/models/GradeCalculatorModel.cs
+++ Mission04_br333/models/GradeCalculatorModel.cs
@@ -8,22 +8,22 @@ namespace Mission04_br333.models
 {
     public class GradeCalculatorModel
     {
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for Assignments.")]
         public double Assignments { get; set; }
         
-        [Range (0, 100, ErrorMessage ="Please enter a number between 0 and 100.")]
+        [Range (0, 100, ErrorMessage ="Please enter a number between 0 and 100 for Group Projects.")]
         public double Group { get; set; }
 
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for Quizzes.")]
         public double Quizzes { get; set; }
 
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for the Midterm Exam.")]
         public double Midterm { get; set; }
 
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for the Final Exam.")]
         public double Final { get; set; }
 
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for INTEX.")]
         public double INTEX { get; set; }
 
 
